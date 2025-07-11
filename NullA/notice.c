adb13a2f2997d391e0e391335c4468e450800927
Preclik <ransdorfd@gmail.com>
2025-07-11 08:34:08 +0200

    Delete NullA directory

--- NullA/notice.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Your network has beem shuted down becouse you got 5+ pings/s\n");
-    printf("This may have been caused becouse of Dos or DDoS attack\n\n");
-    return 0;
-}
